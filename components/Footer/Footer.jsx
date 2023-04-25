89815adbaa64a9936b63271b7138c78b5e963f93
Faith Adebayo <63503378+Dev-Faith@users.noreply.github.com>
2023-04-25 21:58:04 +0100

    added Link tag to the footer links

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import {logo} from "../../assets";
 import {BsFacebook, BsTwitter, BsTelegram, BsYoutube, BsDiscord, BsReddit, BsGithub, BsInstagram} from "react-icons/bs";
 import {FaLinkedinIn} from "react-icons/fa";
 import {SiCoinmarketcap} from "react-icons/si";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./Footer.module.css";
 export default function Footer () {
@@ -13,68 +14,68 @@ export default function Footer () {
             <div className={styles.column}>
               <p className={styles.header}>Corporate</p>
               <div className={styles.links}>
-                <p>About Us</p>
-                <p>Join us</p>
-                <p>Media kit</p>
-                <p>Afiliate Program</p>
-                <p>Whistleblower Contact</p>
-                <p>Blog</p>
-                <p>News & Announcements</p>
-                <p>Referral</p>
-                <p>Security</p>
+                <Link href ="#"><p>About Us</p></Link>
+                <Link href ="#"><p>Join us</p></Link>
+                <Link href ="#"><p>Media kit</p></Link>
+                <Link href ="#"><p>Afiliate Program</p></Link>
+                <Link href ="#"><p>Whistleblower Contact</p></Link>
+                <Link href ="#"><p>Blog</p></Link>
+                <Link href ="#"><p>News & Announcements</p></Link>
+                <Link href ="#"><p>Referral</p></Link>
+                <Link href ="#"><p>Security</p></Link>
               </div>
             </div>
             <div className={styles.column}>
               <p className={styles.header}>Products</p>
               <div className={styles.links}>
-                <p>Spot Trading</p>
-                <p>Margin Trading</p>
-                <p>Convert</p>
-                <p>Futures Trading</p>
-                <p>VeriCoin Earn</p>
-                <p>Crypto Lending</p>
-                <p>Calculator</p>
-                <p>Windvane NFT Marketplace</p>
-                <p>Halo Wallet</p>
+                <Link href ="#"><p>Spot Trading</p></Link>
+                <Link href ="#"><p>Margin Trading</p></Link>
+                <Link href ="#"><p>Convert</p></Link>
+                <Link href ="#"><p>Futures Trading</p></Link>
+                <Link href ="#"><p>VeriCoin Earn</p></Link>
+                <Link href ="#"><p>Crypto Lending</p></Link>
+                <Link href ="#"><p>Calculator</p></Link>
+                <Link href ="#"><p>Windvane NFT Marketplace</p></Link>
+                <Link href ="#"><p>Halo Wallet</p></Link>
               </div>
             </div>
             <div className={styles.column}>
               <p className={styles.header}>Service</p>
               <div className={styles.links}>
-                <p>Help Center</p>
-                <p>Submit a Ticket</p>
-                <p>Fees</p>
-                <p>VIP</p>
-                <p>Identity Verification</p>
-                <p>Technical Support</p>
-                <p>Beginner's Guide</p>
-                <p>Official Verification Center</p>
-                <p>PoR (Proof of Reserves)</p>
-                <p>Sitemap</p>
+                <Link href ="#"><p>Help Center</p></Link>
+                <Link href ="#"><p>Submit a Ticket</p></Link>
+                <Link href ="#"><p>Fees</p></Link>
+                <Link href ="#"><p>VIP</p></Link>
+                <Link href ="#"><p>Identity Verification</p></Link>
+                <Link href ="#"><p>Technical Support</p></Link>
+                <Link href ="#"><p>Beginner's Guide</p></Link>
+                <Link href ="#"><p>Official Verification Center</p></Link>
+                <Link href ="#"><p>PoR (Proof of Reserves)</p></Link>
+                <Link href ="#"><p>Sitemap</p></Link>
               </div>
             </div>
             <div className={styles.column}>
               <p className={styles.header}>Business</p>
               <div className={styles.links}>
-                <p>Token Listing</p>
-                <p>VeriCoin Labs</p>
-                <p>VeriCoin Ventures</p>
-                <p>Spotlight Application</p>
-                <p>Market Maker Incentive Program</p>
-                <p>P2P Merchant Apllication </p>
-                <p>Safeguard Program</p>
+                <Link href ="#"><p>Token Listing</p></Link>
+                <Link href ="#"><p>VeriCoin Labs</p></Link>
+                <Link href ="#"><p>VeriCoin Ventures</p></Link>
+                <Link href ="#"><p>Spotlight Application</p></Link>
+                <Link href ="#"><p>Market Maker Incentive Program</p></Link>
+                <Link href ="#"><p>P2P Merchant Apllication </p></Link>
+                <Link href ="#"><p>Safeguard Program</p></Link>
               </div>
             </div>
             <div className={styles.column}>
               <p className={styles.header}>Legal</p>
               <div className={styles.links}>
-                <p>Terms of Use</p>
-                <p>Privacy Policy</p>
-                <p>Risk Disclosure Statement</p>
-                <p>AML & CFT</p>
-                <p>Special Treatment</p>
-                <p>VeriCoin Plus</p>
-                <p>Law Enforcement Requests</p>
+                <Link href ="#"><p>Terms of Use</p></Link>
+                <Link href ="#"><p>Privacy Policy</p></Link>
+                <Link href ="#"><p>Risk Disclosure Statement</p></Link>
+                <Link href ="#"><p>AML & CFT</p></Link>
+                <Link href ="#"><p>Special Treatment</p></Link>
+                <Link href ="#"><p>VeriCoin Plus</p></Link>
+                <Link href ="#"><p>Law Enforcement Requests</p></Link>
               </div>
             </div>
           </div>
@@ -82,40 +83,40 @@ export default function Footer () {
               <div className={styles.column}>
                 <p className={styles.header}>Crypto Prices</p>
                 <div className={styles.links}>
-                  <p>Bitcoin (BTC) Price</p>
-                  <p>Ethereum (ETH) Price</p>
-                  <p>Ripple (XRP) Price</p>
-                  <p>VeriCoin Token (VCS) Price</p>
+                  <Link href ="#"><p>Bitcoin (BTC) Price</p></Link>
+                  <Link href ="#"><p>Ethereum (ETH) Price</p></Link>
+                  <Link href ="#"><p>Ripple (XRP) Price</p></Link>
+                  <Link href ="#"><p>VeriCoin Token (VCS) Price</p></Link>
                 </div>
               </div>
               <div className={styles.column}>
                 <p className={styles.header}>Learn</p>
                 <div className={styles.links}>
-                  <p>Buy Bitcoin</p>
-                  <p>Buy Ethereum</p>
-                  <p>Buy FLOKI</p>
-                  <p>Buy XRP</p>
-                  <p>Buy Solana</p>
-                  <p>Buy Ton</p>
-                  <p>Buy Injective Protocol</p>
-                  <p>Buy Arbitrum</p>
-                  <p>Buy Polygon</p>
-                  <p>Buy Fantom</p>
+                  <Link href ="#"><p>Buy Bitcoin</p></Link>
+                  <Link href ="#"><p>Buy Ethereum</p></Link>
+                  <Link href ="#"><p>Buy FLOKI</p></Link>
+                  <Link href ="#"><p>Buy XRP</p></Link>
+                  <Link href ="#"><p>Buy Solana</p></Link>
+                  <Link href ="#"><p>Buy Ton</p></Link>
+                  <Link href ="#"><p>Buy Injective Protocol</p></Link>
+                  <Link href ="#"><p>Buy Arbitrum</p></Link>
+                  <Link href ="#"><p>Buy Polygon</p></Link>
+                  <Link href ="#"><p>Buy Fantom</p></Link>
                 </div>
               </div>
               <div className={styles.column}>
                 <p className={styles.header}>Developer</p>
                 <div className={styles.links}>
-                  <p>API Documentation</p>
-                  <p>SDK</p>
-                  <p>Sandbox</p>
+                  <Link href ="#"><p>API Documentation</p></Link>
+                  <Link href ="#"><p>SDK</p></Link>
+                  <Link href ="#"><p>Sandbox</p></Link>
                 </div>
               </div>
               <div className={styles.column}>
                 <p className={styles.header}>App Download</p>
                 <div className={styles.links}>
-                  <p>Android Download</p>
-                  <p>iOS Download</p>
+                  <Link href ="#"><p>Android Download</p></Link>
+                  <Link href ="#"><p>iOS Download</p></Link>
                 </div>
               </div>
               <div className={styles.column}>
@@ -137,7 +138,7 @@ export default function Footer () {
         </div>
         <div className={styles.separator}></div>
         <div className={styles.bottomPart}>
-          <p>CopyRight © 2017 - 2023 VeriCoin.com. All Rights Reserved.</p>
+         <p>CopyRight © 2017 - 2023 VeriCoin.com. All Rights Reserved.</p>
         </div>
       </div>
     );
