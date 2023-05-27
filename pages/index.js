b7ade688ce2a3c7ac415665c1271e4f70ea8bc62
SardiusJay <abidoyejay@gmail.com>
2023-05-27 15:14:40 +0100

    Del:delet files not needed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '@/styles/Home.module.css';
+
 
 export default function Home() {
   return (
@@ -10,7 +10,10 @@ export default function Home() {
         <meta name="description" content="A coin-app Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <title>Coin-App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,300;1,900&family=Outfit:wght@400;700&family=Poppins:ital,wght@0,400;0,500;0,700;1,500&display=swap" rel="stylesheet"></link>
+        <title>Vericoin</title>
       </Head>
       <main className={styles.main}>
         
